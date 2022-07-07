392181f4657c58ce911fb169478ab448d98e7acb
Felix-hans <fhans@wewyse.com>
2022-07-07 11:01:42 +0200

    moved folders

--- Preprocessing/main_preprocessing.py
+++ Preprocessing/main_preprocessing.py
@@ -2,13 +2,12 @@ import seasonality
 import pandas as pd
 
 #import data - we define a list of unique weeks that are subject to event & seasonality engineering
-df = pd.read_csv('data/FRA_SPEND_MEDIA_EXECUTION_MAPPING.csv')
+df = pd.read_csv('../data/FRA_SPEND_MEDIA_EXECUTION_MAPPING.csv')
 unique_weeks = pd.DataFrame(df['YEAR_WEEK'].unique())
 unique_weeks = unique_weeks.rename(columns={0:'year_week'})
 
 
 def run():
-    print(unique_weeks)
 
     #add event & seasonality features
     features_df = seasonality.construct_seasonality_and_event_features(
